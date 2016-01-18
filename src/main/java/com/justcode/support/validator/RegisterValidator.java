c52975c4eadde3442cecf7fbe4727b0c67af6631
Bartek <bartek.ciurus@gmail.com>
2016-01-18 19:18:34 +0100

    Register validation

--- src/main/java/com/justcode/support/validator/RegisterValidator.java
+++ src/main/java/com/justcode/support/validator/RegisterValidator.java
@@ -3,12 +3,23 @@ package com.justcode.support.validator;
 import com.justcode.service.UserService;
 import com.justcode.support.data.PostData;
 import com.justcode.support.data.RegisterData;
+
 import java.util.Locale;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 
 public class RegisterValidator implements FormValidator {
+
+    public static final String ERRORS_PASSWORD_SHORT = "errors.password.short";
+    public static final String ERRORS_FIELDS_EMPTY = "errors.fields.empty";
+    public static final String PASSWORD_ARE_NOT_THE_SAME_ERROR = "errors.password.not.same";
+    public static final String PASSWORD_EMPTY_ERROR = "errors.password.empty";
+    public static final String USERNAME_EMPTY_ERROR = "errors.username.empty";
+    public static final String EMAIL_EMPTY_ERROR = "errors.email.empty";
+    public static final String USERNAME_ALREADY_TAKEN_ERROR = "errors.username.taken";
+    public static final String EMAIL_ALREADY_IN_USE = "errors.email.taken";
 
     @Autowired
     private MessageSource messageSource;
@@ -18,21 +29,16 @@ public class RegisterValidator implements FormValidator {
     private ValidateResult validateResult;
     private Locale locale;
 
-    public static final String PASSWORD_ARE_NOT_THE_SAME_ERROR = "errors.password.not.same";
-    public static final String PASSWORD_EMPTY_ERROR = "errors.password.empty";
-    public static final String USERNAME_EMPTY_ERROR = "errors.username.empty";
-    public static final String EMAIL_EMPTY_ERROR = "errors.email.empty";
-    public static final String USERNAME_ALREADY_TAKEN_ERROR = "errors.username.taken";
-    public static final String EMAIL_ADREADY_IN_USE = "errors.email.taken";
-
     public ValidateResult validate(PostData postData, Locale locale) {
         RegisterData registerData = (RegisterData) postData;
         validateResult = new ValidateResult();
         this.locale = locale;
 
+        validateIsPasswordNotTooShort(registerData.getPassword());
         validateArePasswordsSameAndNotEmpty(registerData.getPassword(), registerData.getRepeatedPassword());
         validateIsEmailUsed(registerData.getEmail());
         validateIsUsernameUsed(registerData.getUsername());
+        validateAreFieldsNotEmpty(registerData);
 
         return validateResult;
     }
@@ -43,7 +49,7 @@ public class RegisterValidator implements FormValidator {
             validateResult.setMessage(messageSource.getMessage(USERNAME_EMPTY_ERROR, null, locale));
         }
 
-        if (userService.getUserByName(username) != null){
+        if (userService.getUserByName(username) != null) {
             validateResult.setIsValid(false);
             validateResult.setMessage(messageSource.getMessage(USERNAME_ALREADY_TAKEN_ERROR, null, locale));
         }
@@ -55,9 +61,9 @@ public class RegisterValidator implements FormValidator {
             validateResult.setMessage(messageSource.getMessage(EMAIL_EMPTY_ERROR, null, locale));
         }
 
-        if (userService.getUserByEmail(email) != null){
+        if (userService.getUserByEmail(email) != null) {
             validateResult.setIsValid(false);
-            validateResult.setMessage(messageSource.getMessage(EMAIL_ADREADY_IN_USE, null, locale));
+            validateResult.setMessage(messageSource.getMessage(EMAIL_ALREADY_IN_USE, null, locale));
         }
 
     }
@@ -72,5 +78,21 @@ public class RegisterValidator implements FormValidator {
             validateResult.setIsValid(false);
             validateResult.setMessage(messageSource.getMessage(PASSWORD_ARE_NOT_THE_SAME_ERROR, null, locale));
         }
+    }
+
+    private void validateIsPasswordNotTooShort(String password) {
+        if (password.length() < 6) {
+            validateResult.setIsValid(false);
+            validateResult.setMessage(messageSource.getMessage(ERRORS_PASSWORD_SHORT, null, locale));
+        }
+    }
+
+    private void validateAreFieldsNotEmpty(RegisterData registerData) {
+        if (StringUtils.isBlank(registerData.getEmail()) || StringUtils.isBlank(registerData.getEmail())
+                || StringUtils.isBlank(registerData.getEmail()) || StringUtils.isBlank(registerData.getEmail())) {
+            validateResult.setIsValid(false);
+            validateResult.setMessage(messageSource.getMessage(ERRORS_FIELDS_EMPTY, null, locale));
+        }
+
     }
 }
